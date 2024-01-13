517226f9649900ce76c375e734a01777aafec28d
Nirakar Jena <jenashubham60@gmail.com>
2024-01-13 15:30:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! from DMCE'
 
 if __name__ == '__main__':
     # Run the app on the default host '0.0.0.0' and port 5000
